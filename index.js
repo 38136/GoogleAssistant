6ebd444814d0d3511b37c980eedb99153b102343
38136 <38136.hexaware@gmail.com>
2017-09-13 11:37:12 +0530

    try 1

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 process.env.DEBUG = 'actions-on-google:*';
 
 const Assistant = require('actions-on-google').ApiAiApp;
+var FlightStatsAPI = require('flightstats')
 var express = require('express');
 var bodyParser = require('body-parser');
 var request_lib = require('request'); // for sending the http requests to Numbers API
 var assert = require('assert');
+var rp = require('request-promise');
 
 var app = express();
 
@@ -15,11 +17,40 @@ app.use(bodyParser.json({
 
 // get by action
 
+// var api = new FlightStatsAPI({
+//     appId: '6aac18a6',
+//     apiKey: '40a7e359cb020a07ead5159c2d5d8162',
+//     // optional, defaults to `node flightstats/{package.version}` 
+//     userAgent: 'Flight_Bot',
+// })
+
 const TrackByFlight_ID = "TrackByFlightID";
 const TrackByStarting_Date = "TrackByStartingDate";
 const Help_Intent = "HelpIntent";
 const WelcomeIntent = "input.welcome";
 const quit_Intent = "quit_Intent";
+
+// Options are optional; 
+// defaults to retrieve all currently active airlines 
+// api.getAirlines(options, callback)
+// // Options (iata, icao, fs are mutually exclusive): 
+// var options = {
+//     all: {
+//         Boolean
+//     },
+//     date: {
+//         Date
+//     },
+//     iata: {
+//         String
+//     },
+//     icao: {
+//         String
+//     },
+//     fs: {
+//         String
+//     },
+// }
 
 app.post('/', function (req, res) {
     const assistant = new Assistant({
@@ -35,15 +66,51 @@ app.post('/', function (req, res) {
         assistant.ask(reply);
     }
 
-    function provideDetailsByID(assistant) {
-        let flightNumber = assistant.getArgument('flightNumber');
-        if (flightNumber !== "") {
-            console.log("flight number is" + flightNumber);
-            assistant.ask("Flight is on the way of lat and log");
+
+    function provideDetailsByID(request, response) {
+        var flightNumber_url = assistant.getArgument('flightNumber');
+        console.log("the response is " + response);
+        if (flightNumber_url) {
+            var p = Promise.resolve();
+            var getDetails = {
+                method: 'GET',
+                // 933427129 flight number
+                // uri: `https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/track/933427129?appId=6aac18a6&appKey=40a7e359cb020a07ead5159c2d5d8162&includeFlightPlan=false&maxPositions=2`,
+                uri: `https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/track/${flightNumber_url}?appId=6aac18a6&appKey=40a7e359cb020a07ead5159c2d5d8162&includeFlightPlan=false&maxPositions=2`,
+                json: true,
+                resolveWithFullResponse: true,
+            };
+            console.log("get details log " + getDetails);
+            p = rp(getDetails)
+                .then(function (res) {
+                    let flightId = res.body.request.flightId.requested;
+                    let maxPositions = res.body.request.maxPositions.requested;
+                    let fLNumber = res.body.flightTrack.flightNumber;
+                    let carrierCode = res.body.flightTrack.carrierFsCode;
+                    let departureDate = res.body.flightTrack.departureDate.dateLocal;
+                    let airName = res.body.appendix.airlines[0].name;
+                    let airPortName = res.body.appendix.airports[0].name;
+                    let airPortCity = res.body.appendix.airports[0].city;
+                    let airPortCountryName = res.body.appendix.airports[0].countryName;
+                    let airPortregionName = res.body.appendix.airports[0].regionName;
+                    let airPortlat = res.body.appendix.airports[0].latitude;
+                    let airPortlong = res.body.appendix.airports[0].longitude;
+
+                    console.log("logging flight id " + flightId);
+
+                    FlightTrackdata = `Your flight Id is ${flightId}  the maximum positions is ${maxPositions}  and flight number is ${fLNumber} the carrier code is  ${carrierCode} and the departure date is today and the airport name is ${airPortName} and the airport city name is ${airPortCity} and the country name is ${airPortCountryName} the lattitude are ${airPortlat} logitude is ${airPortlong} `;
+                    assistant.ask(FlightTrackdata);
+                    //  response.say(JSON.stringify(res));
+                    response.send();
+                });
+            return p;
         } else {
-            assistant.ask("Tell me your flight number please");
+            assistant.ask("please tell me your Flight Number");
         }
     }
+
+
+
 
     let actionMap = new Map();
     let actionSee = actionMap.get(TrackByFlight_ID);
@@ -54,25 +121,10 @@ app.post('/', function (req, res) {
     actionMap.set(WelcomeIntent, WelcomeSpeach);
     assistant.handleRequest(actionMap);
 
-
-    // function responseHandler(assistant) {
-    //     console.log("okok")
-    //     // intent contains the name of the intent you defined in the Actions area of API.AI
-    //     let intent = assistant.getIntent();
-    //     switch (intent) {
-    //         case WelcomeIntent:
-    //             assistant.ask('Welcome! Say a number.');
-    //             break;
-
-    //             // case quit_Intent:
-    //             //     let number = assistant.getArgument(NUMBER_ARGUMENT);
-    //             //     assistant.tell('You said ' + number);
-    //             //     break;
-    //     }
-    // }
-    // assistant.handleRequest(responseHandler);
-
 });
+
+
+
 
 app.get('/', function (req, res) {
     res.send("Server is up and running.")
