359967e90396d0042b5a872cec4fdf351949b32a
38136 <38136.hexaware@gmail.com>
2017-09-13 13:33:59 +0530

    try 1

--- index.js
+++ index.js
@@ -62,10 +62,9 @@ app.post('/', function (req, res) {
 
     function WelcomeSpeach(assistant) {
         console.log("this is assistant" + assistant);
-        var reply = "Welcome to Flight Track.. give me you flight number will let you know currently where the flight is";
+        var reply = "Welcome to FlightStat.. give me you flight number will let you know currently where the flight is";
         assistant.ask(reply);
     }
-
 
     function provideDetailsByID(request, response) {
         var flightNumber_url = assistant.getArgument('flightNumber');
@@ -98,7 +97,7 @@ app.post('/', function (req, res) {
 
                     console.log("logging flight id " + flightId);
 
-                    FlightTrackdata = `Your flight Id is ${flightId}  the maximum positions is ${maxPositions}  and flight number is ${fLNumber} the carrier code is  ${carrierCode} and the departure date is today and the airport name is ${airPortName} and the airport city name is ${airPortCity} and the country name is ${airPortCountryName} the lattitude are ${airPortlat} logitude is ${airPortlong} `;
+                    FlightTrackdata = `Your flight Id is ${flightId}  the maximum positions is ${maxPositions}  and flight number is ${fLNumber} the carrier code is  ${carrierCode} and the departure date is today and the airport name is ${airPortName} and the airport city name is ${airPortCity} and the country name is ${airPortCountryName} the lattitude are ${airPortlat} logitude is ${airPortlong}. Do you want to continue. `;
                     assistant.ask(FlightTrackdata);
                     //  response.say(JSON.stringify(res));
                     response.send();
@@ -108,6 +107,48 @@ app.post('/', function (req, res) {
             assistant.ask("please tell me your Flight Number");
         }
     }
+    // ---------------------------------------search by date------------------
+    // function provideDetailsByDate(request, response) {
+    //     var flightNumber_url = assistant.getArgument('flightNumber');
+    //     console.log("the response is " + response);
+    //     if (flightNumber_url) {
+    //         var p = Promise.resolve();
+    //         var getDetails = {
+    //             method: 'GET',
+    //             // 933427129 flight number
+    //             // https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/tracks/AA/100/arr/2017/09/14?appId=6aac18a6&appKey=40a7e359cb020a07ead5159c2d5d8162&utc=false&includeFlightPlan=false&maxPositions=2
+    //             uri: `https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/track/${flightNumber_url}?appId=6aac18a6&appKey=40a7e359cb020a07ead5159c2d5d8162&includeFlightPlan=false&maxPositions=2`,
+    //             json: true,
+    //             resolveWithFullResponse: true,
+    //         };
+    //         console.log("get details log " + getDetails);
+    //         p = rp(getDetails)
+    //             .then(function (res) {
+    //                 let flightId = res.body.request.flightId.requested;
+    //                 let maxPositions = res.body.request.maxPositions.requested;
+    //                 let fLNumber = res.body.flightTrack.flightNumber;
+    //                 let carrierCode = res.body.flightTrack.carrierFsCode;
+    //                 let departureDate = res.body.flightTrack.departureDate.dateLocal;
+    //                 let airName = res.body.appendix.airlines[0].name;
+    //                 let airPortName = res.body.appendix.airports[0].name;
+    //                 let airPortCity = res.body.appendix.airports[0].city;
+    //                 let airPortCountryName = res.body.appendix.airports[0].countryName;
+    //                 let airPortregionName = res.body.appendix.airports[0].regionName;
+    //                 let airPortlat = res.body.appendix.airports[0].latitude;
+    //                 let airPortlong = res.body.appendix.airports[0].longitude;
+
+    //                 console.log("logging flight id " + flightId);
+
+    //                 FlightTrackByDatedata = `Your flight Id is ${flightId}  the maximum positions is ${maxPositions}  and flight number is ${fLNumber} the carrier code is  ${carrierCode} and the departure date is today and the airport name is ${airPortName} and the airport city name is ${airPortCity} and the country name is ${airPortCountryName} the lattitude are ${airPortlat} logitude is ${airPortlong} `;
+    //                 assistant.ask(FlightTrackByDatedata);
+    //                 //  response.say(JSON.stringify(res));
+    //                 response.send();
+    //             });
+    //         return p;
+    //     } else {
+    //         assistant.ask("please tell me your Flight Number");
+    //     }
+    // }
 
 
 
